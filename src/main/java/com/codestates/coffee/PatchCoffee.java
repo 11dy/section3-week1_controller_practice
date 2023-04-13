57c61931a59bff53b75156680c45ad8725b672be
Lee dong yeol <dlehdduf09020@naver.com>
2023-04-13 17:12:43 +0900

    PostCoffee, PatchCoffee DTO 생성(2)

---
+++ src/main/java/com/codestates/coffee/PatchCoffee.java
@@ -0,0 +1,31 @@
+package com.codestates.coffee;
+
+public class PatchCoffee {
+    private Long coffeeId;
+    private String korName;
+    private int price;
+
+    public Long getCoffeeId() {
+        return coffeeId;
+    }
+
+    public void setCoffeeId(Long coffeeId) {
+        this.coffeeId = coffeeId;
+    }
+
+    public String getKorName() {
+        return korName;
+    }
+
+    public void setKorName(String korName) {
+        this.korName = korName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
