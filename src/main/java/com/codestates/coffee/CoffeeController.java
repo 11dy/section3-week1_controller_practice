7fadd4eb85529c2a790cd68ce188ebff11fefe77
Lee dong yeol <dlehdduf09020@naver.com>
2023-04-13 17:04:42 +0900

    PostCoffee, PatchCoffee DTO 생성.

--- src/main/java/com/codestates/coffee/CoffeeController.java
+++ src/main/java/com/codestates/coffee/CoffeeController.java
@@ -4,23 +4,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/v1/coffees")
 public class CoffeeController {
     // 커피 정보 등록
     @PostMapping
-    public ResponseEntity postCoffee(@RequestParam("engName") String engName,
-                                     @RequestParam("korName") String korName,
-                                     @RequestParam("price") int price){
-        Map<String, Object> map = new HashMap<>();
-        map.put("engName", engName);
-        map.put("korName", korName.trim()); // 한글 입력시 양 끝의 공백 제거
-        map.put("price", Integer.valueOf(price));
+    public ResponseEntity postCoffee(@RequestBody PostCoffee postCoffee){
+        return new ResponseEntity<>(postCoffee, HttpStatus.CREATED);
+    }
 
-        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    // 커피 수정
+    @PatchMapping("/{coffee-id}")
+    public ResponseEntity patchCoffee(@PathVariable("coffee-id") long coffeeId,
+                                      @RequestBody PatchCoffee patchCoffee){
+        patchCoffee.setPrice((int)coffeeId);
+
+        return new ResponseEntity<>(patchCoffee, HttpStatus.OK);
     }
 
     // 특정 커피 정보를 제공하는 핸들러 메서드
@@ -37,6 +36,14 @@ public class CoffeeController {
         System.out.println("# get Coffees");
         // not implementation
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // 커피 삭제
+    @DeleteMapping("/{coffee-id}")
+    public ResponseEntity deleteCoffee(@PathVariable("coffee-id") long coffeeId) {
+        // No need business logic
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
 
